3c15b4d139958b9c7a8c4c9e29472a3bd787585b
Maxence Cuillierier <maxence.cuillierier@mmibordeaux.com>
2022-03-31 18:55:50 +0200

    problème carte google map

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Destination.create!([
+  { "pseudo": "Buckingham Palace","latitude": "51.501564","longitude": "-0.141944", "message": "okokokoko"},
+  { "pseudo": "Buckingham Palace","latitude": "51.501564","longitude": "-0.141944", "message": "okokokoko"},
+  { "pseudo": "Buckingham Palace","latitude": "51.501564","longitude": "-0.141944", "message": "okokokoko"},
+  { "pseudo": "Buckingham Palace","latitude": "51.501564","longitude": "-0.141944", "message": "okokokoko"}
+  ])
